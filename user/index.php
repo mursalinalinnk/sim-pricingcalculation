877807b33d1cdf67bf4a750054ed8b17a463ac6d
gen <genkerja@gmail.com>
2016-04-11 22:23:37 +0100

    update

--- user/index.php
+++ user/index.php
@@ -106,9 +106,13 @@ include "../include/config.php";
 
 	</div>
 
-	<ul class="nav navbar-nav navbar-right">			 
-			<li><a href="pend.php">visit website</a></li>
-	</ul>
+	<nav class="navbar navbar-deault navbar-fixed-top">
+	<div class="container">
+		<ul>
+			<li class="navbar-btn navbar-right"><a href="www.youtube.com"><button type="button" class="btn btn-default ">Visit website</button></a></li>
+		</ul>
+	</div>
+	</nav>
 
 </nav>
 
@@ -193,20 +197,17 @@ include "../include/config.php";
 
 	<!-- TABEL -->
 	<div class="row">
-            <div class="col-lg-8">
-                <h1 class="page-header">Tables</h1>
-            </div>
-            <div class="col-lg-4">
-                <h1 class="page-header">Tweet</h1>
+            <div class="col-lg-12">
+                <h1 class="page-header">kalkulasi harga</h1>
             </div>
                 <!-- /.col-lg-9+3 -->
-            </div>
+
             <!-- /.row -->
             <div class="row" >
-                <div class="col-lg-8">
+                <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <h3>Daftar Kebutuhan Bulanan</h3>
+                            <h3>pilih ukuran</h3>
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
@@ -289,83 +290,6 @@ include "../include/config.php";
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.panel -->
-
-
-                    <div class="panel panel-default">
-		               	<div class="panel-heading">
-                            <h3>Daftar Kebutuhan sekunder</h3>
-                        </div>
-                        <div class="panel-body">
-                            <div class="dataTable_wrapper">
-								<table class="table table-striped table-bordered table-hover";>
-									<thead>
-									<tr>
-										<!-- <th style="border: 1px solid black"; >no</th> -->
-										<th style="border: 1px solid black"; >#</th>
-										<th style="border: 1px solid black";>nama</th>
-										<th style="border: 1px solid black";>jumlah</th>
-										<th style="border: 1px solid black";>harga</th>
-										<th style="border: 1px solid black";>note</th>
-									</tr>
-									<?php
-									tampilksek();
-									?>
-									</thead>
-								</table>
-							</div>
-
-							<!-- /.table-responsive -->
-                            <div class="well">
-                                <h4>Cara menambahkan dan mengurangi kebutuhan pokok</h4>
-                                <p>cukup isikan data seperti biasa lalu click tombol "action" yang di inginkan.khusus untuk menambah anda tidak perlu mengisi kode kebutuhan karna terisi secara otomatis</a>.</p>
-                                <a class="btn btn-default btn-lg btn-block" target="_blank" href="https://datatables.net/">View DataTables Documentation</a>
-                            </div>
-
-	                        <div class="col-md-12">
-								<form action="../include/prosescruduser.php" method="POST">
-								<table>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label>#kode</label>
-											<input type="text" class="form-control" name="kode_ksek" placeholder="hanya untuk hapus/update">
-										</div>
-									
-											<!-- <label>tersembunyi kode kpok</label> -->
-											<input type="hidden" class="form-control" name="uname_userksek" value="<?php echo $user->uname_user;?>">
-								
-										<div class="form-group">
-											<label>kebutuhan</label>
-											<input type="text" class="form-control" name="nama_ksek" placeholder="beli tas baru">
-										</div>
-										<div class="form-group">
-											<label>jumlah</label>
-											<input type="text" class="form-control" name="jumlah_ksek" placeholder="1">
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label>harga</label>
-											<input type="text" class="form-control" type="text" name="harga_ksek" placeholder="Rp.10.000">
-										</div>
-										<div class="form-group">
-											<label>note</label>
-											<textarea class="form-control" rows="5" name="note_ksek" ></textarea>
-										</div>
-									</div>									
-								</table>
-									
-								<input type="submit" class="btn btn-sm btn-success" name="tambahksek" value="simpan">
-								<input type="submit" class="btn btn-sm btn-success"name="updateksek" value="ubah">
-								<input type="submit"class="btn btn-sm btn-success" name="hapusksek" value="hapus">
-								<input type="reset"class="btn btn-sm btn-success">
-								
-								</form>
-							</div>
-
-						</div>
-						<!-- panel body -->	
-		            </div>
-		            <!-- panel default -->
                 </div>
                 <!-- /.col-lg-9 -->
 
@@ -375,38 +299,6 @@ include "../include/config.php";
 
 
         	    <!-- /.panel -->
-        	    <div class="col-md-4">
-                <div class="chat-panel panel panel-success">
-                    <div class="panel-heading">
-                        <form action="../include/tweetcrud.php" method="POST">
-							<div class="form-group">			
-								<input type="hidden" name="kode_tweet">
-								<input type="hidden" name="uname_usertweet" value="<?php echo $user->uname_user;?>">
-								<textarea type="text" name="isi" rows="3" class="form-control" placeholder="bagaimana keuanganmu hari ini ..."></textarea>
-								<input type="hidden" class="form-control" name="tgl_tweet" disabled="">
-								<input type="hidden" name="foto_usertweet" value="<?php echo $user->foto_user;?>">
-							</div>
-							<span class="input-group-btn">
-	                            <button class="btn btn-default btn-circle" type="submit" name="tweet" value="tweet" >
-	                                <i class="fa fa-twitter"></i>
-	                            </button>
-	                            <?php echo "<img class=\"img-rounded img-circle pull-right\" style=\"width 35px;height:35px \" src='user_image/$user->foto_user'>"; ?>
-	                        </span>
-		
-						</form>
-                    </div>
-                    <!-- /.panel-heading -->
-                    <div class="panel-body">
-                        <ul class="chat">
-                            <?php    
-                            
-				        	tampiltweet();
-				        	?>
-                        </ul>
-                    </div>
-                </div>
-                </div>
-                <!-- /.panel .chat-panel -->
             </div>
             <!-- /.row -->
 
